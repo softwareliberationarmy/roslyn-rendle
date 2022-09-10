b094bd62763615d5c3e4a463fc52607b293da094
Kerry Patrick <kpatrick@mmmicrosystems.com>
2022-09-10 11:08:37 -0500

    Rewriting code!

---
+++ StringLiteralUpshifter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+public class StringLiteralUpshifter: CSharpSyntaxRewriter
+{
+    private readonly SemanticModel _model;
+
+    public StringLiteralUpshifter(SemanticModel model)
+    {
+        _model = model;
+    }
+
+    public override SyntaxNode? VisitLiteralExpression(LiteralExpressionSyntax node)
+    {
+        if (_model.GetTypeInfo(node).Type is INamedTypeSymbol type)
+        {
+            if (type.Name == "String" && type.GetMembers(nameof(string.ToUpperInvariant)).FirstOrDefault() != null)
+            {
+                return SyntaxFactory.InvocationExpression(
+                    SyntaxFactory.MemberAccessExpression(
+                        SyntaxKind.SimpleMemberAccessExpression,
+                        node,
+                        SyntaxFactory.IdentifierName(nameof(string.ToUpperInvariant))));
+            }
+        }
+
+        return base.VisitLiteralExpression(node);
+    }
+}
