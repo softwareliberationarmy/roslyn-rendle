ffab468735c3757abb1bf8a97b10b1a864d6a8f8
Kerry Patrick <kpatrick@mmmicrosystems.com>
2022-09-10 10:32:50 -0500

    more symbol walking to find used references

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 Console.WriteLine("Hello, World!");
 
 var demo = new RoslynDemo();
-await demo.Start();
+//await demo.Start("E:\\git\\steris\\spmi\\SPMi.sln");
+await demo.Start("C:\\git\\work\\spmi\\SPMi.sln");
 
+Console.WriteLine("*** DONE ***");
 Console.ReadLine();
